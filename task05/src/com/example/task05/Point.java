b94fbf68650decbea060f18522f6d22ecdba9325
Artem Kuznetsov <artemkuz1083@gmail.com>
2024-10-04 02:41:35 +0500

    Кузнецов Артём. Задание 5

--- task05/src/com/example/task05/Point.java
+++ task05/src/com/example/task05/Point.java
@@ -1,49 +1,25 @@
 package com.example.task05;
 
-/**
- * Точка в двумерном пространстве
- */
 public class Point {
 
-    /**
-     * Конструктор, инициализирующий координаты точки
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
+    private final double X;
+    private final double Y;
+
     public Point(double x, double y) {
-        throw new AssertionError();
+        this.X = x;
+        this.Y = y;
     }
 
-    /**
-     * Возвращает координату точки по оси абсцисс
-     *
-     * @return координату точки по оси X
-     */
     public double getX() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return X;
     }
 
-    /**
-     * Возвращает координату точки по оси ординат
-     *
-     * @return координату точки по оси Y
-     */
     public double getY() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return Y;
     }
 
-    /**
-     * Подсчитывает расстояние от текущей точки до точки, переданной в качестве параметра
-     *
-     * @param point вторая точка отрезка
-     * @return расстояние от текущей точки до переданной
-     */
     public double getLength(Point point) {
-        // TODO: реализовать
-        throw new AssertionError();
+        return Math.sqrt(Math.pow(X - point.X, 2) + (Math.pow(Y - point.Y, 2)));
     }
 
 }
--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,46 +1,35 @@
 package com.example.task05;
 
-/**
- * Ломаная линия
- */
+import java.util.ArrayList;
+
+
 public class PolygonalLine {
 
-    /**
-     * Устанавливает точки ломаной линии
-     *
-     * @param points массив точек, которыми нужно проинициализировать ломаную линию
-     */
+    private final ArrayList<Point> Points;
+
+    public PolygonalLine() {
+        this.Points = new ArrayList<>();
+    }
+
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        for (Point point : points) {
+            addPoint(point);
+        }
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param point точка, которую нужно добавить к ломаной
-     */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        Points.add(new Point(point.getX(), point.getY()));
     }
 
-    /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        Points.add(new Point(x, y));
     }
 
-    /**
-     * Возвращает длину ломаной линии
-     *
-     * @return длину ломаной линии
-     */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0.0;
+        for (int i = 0; i < Points.size() - 1; i++) {
+            length += Points.get(i).getLength(Points.get(i + 1));
+        }
+        return length;
     }
-
 }
